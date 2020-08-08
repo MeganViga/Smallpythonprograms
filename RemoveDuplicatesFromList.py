417ff4ba622a4c6106cd9fe005cd833b576055d5
MeganViga <viganesh.s.m@gmail.com>
2020-08-08 07:23:47 +0530

    second

--- RemoveDuplicatesFromList.py
+++ RemoveDuplicatesFromList.py
@@ -1,4 +1,6 @@
 list1 = [1,1,'viganesh','sugadev','viganesh',2.5, 2.5]
-
-list1 = list(dict.fromkeys(list1))
-print(list1)
+list2 = []
+for i in list1:
+    if i not in list2:
+        list2.append(i)
+print(list2)
